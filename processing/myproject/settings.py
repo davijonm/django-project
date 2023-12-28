d5af532f2e26c49181c92e33e7c3c8d30e49cd48
Davi Mendes <davi26030@gmail.com>
2023-12-28 13:52:41 -0300

    added a postgres database a make the migrations to it

--- processing/myproject/settings.py
+++ processing/myproject/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
